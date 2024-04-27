6491e966eb7c93492c3cc2adf6d2c5f788df3d7e
Fendanadil <yb@blue-green.ch>
2024-04-27 16:40:44 +0200

    add basic menu functionality
    - add menu screen
    - add back button to game screen (on game over)
    - add basic button functionality, no data storage

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get data from storage
+
+        // update highscore and player name
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+}
